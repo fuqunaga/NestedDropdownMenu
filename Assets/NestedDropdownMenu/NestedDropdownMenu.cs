56ffb32f51f8a58d977a85df2703d4d54364f0ad
fuqunaga <fukhid@gmail.com>
2025-08-04 18:35:02 +0900

    refactor

--- Assets/NestedDropdownMenu/NestedDropdownMenu.cs
+++ Assets/NestedDropdownMenu/NestedDropdownMenu.cs
@@ -8,21 +8,17 @@ namespace NestedDropdownMenuSystem
     /// <summary>
     /// DropdownMenu that supports hierarchical submenus based on GenericDropdownMenu.
     /// </summary>
-    /// <remarks>
-    /// ルートメニューはGenericDropdownMenuをほぼそのまま使用し、
-    /// サブメニューはGenericDropdownMenu.DropDown()せずにOuterContainer以下のVisualElementを抜き出して、
-    /// </remarks>
     public class NestedDropdownMenu
     {
-        private readonly Dictionary<string, GenericMenu> _menuTable = new();
+        private readonly Dictionary<string, SingleMenu> _menuTable = new();
 
-        private GenericMenu RootMenu
+        private SingleMenu RootMenu
         {
             get
             {
                 if (!_menuTable.TryGetValue(string.Empty, out var menu))
                 {
-                    menu = new GenericMenu();
+                    menu = new SingleMenu();
                     _menuTable[string.Empty] = menu;
 
                 }
@@ -58,19 +54,21 @@ namespace NestedDropdownMenuSystem
             menu.AddSeparator(label);
         }
 
-        public void DropDown(Rect position, VisualElement targetElement = null, bool anchored = false) =>
-            DoDropdown(position, targetElement, anchored);
+        public void DropDown(Rect position, VisualElement targetElement = null, bool anchored = false)
+        {
+            RootMenu.DropDown(position, targetElement, anchored);
+        }
 
         #endregion
 
 
-        private (GenericMenu menu, string label) GetMenuAndLabel(string itemName)
+        private (SingleMenu menu, string label) GetMenuAndLabel(string itemName)
         {
             var (path, label) = ParseItemNameToPathAndLabel(itemName);
 
             if (!_menuTable.TryGetValue(path, out var menu))
             {
-                menu = new GenericMenu();
+                menu = new SingleMenu();
                 _menuTable[path] = menu;
 
                 var isSubMenu = !string.IsNullOrEmpty(path);
@@ -90,31 +88,13 @@ namespace NestedDropdownMenuSystem
         /// <summary>
         /// サブメニュー用のメニューアイテムを追加 
         /// </summary>
-        private void AddSubMenuItem(GenericMenu menu, GenericMenu subMenu, string label)
+        private static void AddSubMenuItem(SingleMenu menu, SingleMenu subMenu, string label)
         {
             if (menu == null || subMenu == null) return;
 
             const int delayMs = 500;
             menu.AddSubmenuItem(label, delayMs, subMenu);
         }
-
-        // private void ShowSubMenu(GenericDropdownMenu subMenu, VisualElement targetElement)
-        // {
-        //     if (subMenu == null) return;
-        //
-        //     var rootMenu = RootMenu;
-        //     var menuContainer = rootMenu.GetMenuContainer();
-        //
-        //     var rectWorld = targetElement?.worldBound ?? new Rect(Vector2.zero, Vector2.zero);
-        //     var position = menuContainer.WorldToLocal(new Vector2(rectWorld.xMax, rectWorld.yMin));
-        //
-        //     var outerContainer = subMenu.GetOuterContainer();
-        //     var style = outerContainer.style;
-        //     style.left = position.x;
-        //     style.top = position.y;
-        //
-        //     menuContainer.Add(outerContainer);
-        // }
 
 
         private static (string path, string label) ParseItemNameToPathAndLabel(string itemName)
@@ -134,37 +114,5 @@ namespace NestedDropdownMenuSystem
             var name = itemName[(lastSlashIndex + 1)..];
             return (path, name);
         }
-
-
-        private void DoDropdown(Rect rect, VisualElement targetElement = null, bool anchored = false)
-        {
-            var rootMenu = RootMenu;
-            rootMenu.DropDown(rect, targetElement, anchored);
-            // ModifyRootMenuCallbacks(rootMenu);
-        }
-
-
-        /// <summary>
-        /// RootMenuのコールバックを付け替えて、NestedDropdownMenuの動作に合わせる
-        /// </summary>
-        /// <param name="menu"></param>
-        private void ModifyRootMenuCallbacks(GenericDropdownMenu menu)
-        {
-            if (menu == null)　return;
-
-            // OnFocusOutでscrollViewの範囲外にマウスがあるときtにHide()してしまう
-            // これはサブメニュー上にマウスがある場合は閉じないようにしたい
-            // そのためにRootMenuのOnFocusOutイベントを無効化し、自前のイベントを登録する
-            var scrollView = menu.GetScrollView();
-            scrollView.UnregisterCallback(menu.GetOnFocusOutDelegate());
-
-            var rootMenuContainer = menu.GetMenuContainer();
-            // rootMenuContainer.RegisterCallback<PointerDownEvent>(OnPointerDown);
-            // rootMenuContainer.RegisterCallback<PointerMoveEvent>(OnPointerMove);
-            // rootMenuContainer.RegisterCallback<FocusOutEvent>(OnFocusOut);
-            //
-            // rootMenuContainer.RegisterCallback<PointerUpEvent>(OnPointerUp);
-        }
-
     }
 }
---
+++ Assets/NestedDropdownMenu/SingleMenu.cs
@@ -0,0 +1,219 @@
+﻿#nullable  enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UIElements;
+
+namespace NestedDropdownMenuSystem
+{
+    /// <summary>
+    /// Enhanced GenericDropdownMenu with support for hover-delayed submenu popup functionality.
+    /// </summary>
+    /// <remarks>
+    /// GenericDropdownMenuを部分的に使用してサブメニューを表示するための機能を追加したクラス
+    /// ルートメニューのみMenuContainer（画面全体を覆うコンテナ）を使用し、
+    /// サブメニューはOuterContainerをルートメニューのMenuContainerにAddする
+    /// 
+    /// GenericDropdownMenuはポインターのイベントをMenuContainerで受けるがここでは各メニューのOuterContainerで受けるようにしている
+    /// ポインターコールバックの呼び出し順は以下の通り
+    /// - 子要素上にポインターがあればそのポインターイベントが呼ばれる
+    /// - すべての子要素上にポインターがない場合、ルートメニューのMenuContainerに最も子であるメニューのポインターイベントを登録しており、
+    ///   メニュー範囲外のとき選択が解除される
+    /// </remarks>
+    public class SingleMenu : GenericDropdownMenu
+    {
+        #region Access private
+        
+        private static readonly Action<GenericDropdownMenu, PointerDownEvent> OnPointerDownFunc;
+        private static readonly Action<GenericDropdownMenu, PointerMoveEvent> OnPointerMoveFunc;
+        private static readonly Action<GenericDropdownMenu, PointerUpEvent> OnPointerUpFunc;
+
+        private static readonly Func<GenericDropdownMenu, int> GetSelectedIndexFunc;
+        private static readonly Action<GenericDropdownMenu, bool> HideFunc;
+
+        static SingleMenu()
+        {
+            RegisterPrivateMethodToDelegate("OnPointerDown", out OnPointerDownFunc);
+            RegisterPrivateMethodToDelegate("OnPointerMove", out OnPointerMoveFunc);
+            RegisterPrivateMethodToDelegate("OnPointerUp", out OnPointerUpFunc);
+            RegisterPrivateMethodToDelegate("GetSelectedIndex", out GetSelectedIndexFunc);
+            RegisterPrivateMethodToDelegate("Hide", out HideFunc);
+            
+            return;
+
+
+            static void RegisterPrivateMethodToDelegate<TFunc>(string methodName, out TFunc func) 
+                where TFunc : Delegate
+            {
+                var methodInfo = typeof(GenericDropdownMenu).GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+                Assert.IsNotNull(methodInfo, $"Method '{methodName}' not found in GenericDropdownMenu");
+                
+                func = (TFunc)Delegate.CreateDelegate(typeof(TFunc), methodInfo);
+            }
+        }
+        
+        
+        #endregion
+        
+        private static VisualElement GetFirstAncestorByClassName(VisualElement element, string className)
+        {
+            while (element != null)
+            {
+                if (element.ClassListContains(className))
+                {
+                    return element;
+                }
+
+                element = element.parent;
+            }
+
+            throw new InvalidOperationException($"Ancestor with class name '{className}' not found.");
+        }
+
+        
+        private readonly Dictionary<VisualElement, SingleMenu> _itemToSubmenuTable = new();
+        private readonly VisualElement _outerContainer;
+        private readonly VisualElement _scrollView;
+
+        private SingleMenu? _parentMenu;
+        
+        
+        private SingleMenu RootMenu => _parentMenu?.RootMenu ?? this;
+
+        private VisualElement RootMenuContainer => GetFirstAncestorByClassName(RootMenu._outerContainer, ussClassName);
+        
+
+        public SingleMenu()
+        {
+            _outerContainer = GetFirstAncestorByClassName(contentContainer, containerOuterUssClassName);
+            _scrollView = GetFirstAncestorByClassName(contentContainer, containerInnerUssClassName);
+            
+            _outerContainer.RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
+            _outerContainer.RegisterCallback<DetachFromPanelEvent>(OnDetachFromPanel);
+        }
+
+        #region Callbacks
+        
+        private void OnAttachToPanel(AttachToPanelEvent evt)
+        {
+            _outerContainer.RegisterCallback<PointerDownEvent>(OnPointerDown);
+            _outerContainer.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+            _outerContainer.RegisterCallback<PointerUpEvent>(OnPointerUp);
+            
+            var rootMenuContainer = RootMenuContainer;
+            rootMenuContainer.RegisterCallback<PointerDownEvent>(OnPointerDown);
+            rootMenuContainer.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+        }
+
+        private void OnDetachFromPanel(DetachFromPanelEvent evt)
+        {
+            _outerContainer.UnregisterCallback<PointerDownEvent>(OnPointerDown);
+            _outerContainer.UnregisterCallback<PointerMoveEvent>(OnPointerMove);
+            _outerContainer.UnregisterCallback<PointerUpEvent>(OnPointerUp);
+            
+            var rootMenuContainer = RootMenuContainer;
+            rootMenuContainer.UnregisterCallback<PointerDownEvent>(OnPointerDown);
+            rootMenuContainer.UnregisterCallback<PointerMoveEvent>(OnPointerMove);
+        }
+
+
+        private void OnPointerDown(PointerDownEvent evt)
+        {
+            OnPointerDownFunc(this, evt);
+            HideSubmenuIfItemUnselected();
+        }
+
+        private void OnPointerMove(PointerMoveEvent evt)
+        {
+            OnPointerMoveFunc(this, evt);
+            HideSubmenuIfItemUnselected();
+        }
+        
+        private void OnPointerUp(PointerUpEvent evt)
+        {
+            OnPointerUpFunc(this, evt);
+            if (GetSelectedIndexFunc(this) != -1)
+            {
+               HideFunc(RootMenu, true);
+            }
+        }
+
+        #endregion
+        
+        
+        private void HideSubmenuIfItemUnselected()
+        {
+            var selectedItem = GetSelectedItem();
+            if (selectedItem == null) return;
+            
+            foreach(var (item, submenu) in _itemToSubmenuTable)
+            {
+                if ( item != selectedItem )
+                {
+                    submenu.HideAsSubmenu();
+                }
+            }
+        }
+        
+        private VisualElement? GetSelectedItem()
+        {
+            var selectedIndex = GetSelectedIndexFunc(this);
+            if (selectedIndex < 0 || selectedIndex >= _scrollView.childCount)
+            {
+                return null;
+            }
+            
+            return _scrollView.Children()
+                .Where(ve => ve.ClassListContains(itemUssClassName))
+                .ElementAt(selectedIndex);
+        }
+        
+
+        public void AddSubmenuItem(string itemName, long delayMs, SingleMenu subMenu)
+        {
+            AddItem(itemName, false, null);
+            var item = contentContainer.Children().Last();
+            
+            _itemToSubmenuTable[item] = subMenu;
+            
+            // PointerEnterして一定時間経過後にサブメニューを表示する
+            // PointerLeaveしたら時間計測をストップ
+            IVisualElementScheduledItem? scheduledItem = null;
+
+            item.RegisterCallback<PointerEnterEvent>(_ =>
+            {
+                scheduledItem ??= item.schedule.Execute(() => subMenu.ShowAsSubmenu(this, item));
+                scheduledItem.ExecuteLater(delayMs);
+            });
+
+            item.RegisterCallback<PointerLeaveEvent>(_ => { scheduledItem?.Pause(); });
+        }
+
+        
+        private void ShowAsSubmenu(SingleMenu parentMenu, VisualElement targetElement)
+        {
+            _parentMenu = parentMenu;
+            
+            // TODO: ほかのサブメニューも考慮
+            var rectWorld = targetElement.worldBound;
+            var position = RootMenuContainer.WorldToLocal(new Vector2(rectWorld.xMax, rectWorld.yMin));
+            
+            var style = _outerContainer.style;
+            style.left = position.x;
+            style.top = position.y;
+            
+            RootMenuContainer.Add(_outerContainer);
+        }
+
+        private void HideAsSubmenu()
+        {
+            _outerContainer.RemoveFromHierarchy();
+            _parentMenu = null;
+        }
+    }
+}
+        
